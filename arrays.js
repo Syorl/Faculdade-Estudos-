1562781d6d5671a9a3e181575dbff03597f3d1f9
Guilherme <guiornellas14@gmail.com>
2024-09-02 20:04:42 -0300

    QQQQ

---
+++ arrays.js
@@ -0,0 +1,15 @@
+let pessoa = [];
+pessoa = ["Guilherme"]; //criar um array [0,1,2,3,4,5,6,7,8,9,10] valor 0 é o primeiro da que está dentro do array, arrays funcionam
+//como um conjuto de variaveis cada valor é uma variavel
+pessoa.push(19); // add no array
+pessoa.pop(19); // remover do array
+pessoa.push(18);
+pessoa.push("programador");
+console.log(
+    " A pessoa chamada " +
+    pessoa[0] +
+    " tem cerca de " +
+    pessoa[1] +
+    " anos de idade e sonha em ser um " +
+    pessoa[2]
+);
